d5041371a1233e2426d07bcdc70d7a837e6fb78e
Enry679 <91887586+Enry679@users.noreply.github.com>
2021-11-14 20:16:52 +0100

    renaming

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,23 +1,19 @@
 const hre = require("hardhat");
 
-function tokens(n) {
-  return ethers.utils.parseEther(n);
-}
-
 async function main() {
   const owner = process.env.OWNER;
   const whitelister = process.env.WHITELISTER;
 
   const HappyFun = await hre.ethers.getContractFactory("HappyFansToken");
-  const happyFun = await HappyFun.deploy();
+  const happyFans = await HappyFun.deploy();
 
-  await happyFun.deployed();
+  await happyFans.deployed();
 
-  await happyFun.transfer(owner, tokens('100000000000'));
-  await happyFun.transferOwnership(owner);
-  await happyFun.transferWhitelister(whitelister);
+  await happyFans.transfer(owner, tokens('100000000000'));
+  await happyFans.transferOwnership(owner);
+  await happyFans.transferWhitelister(whitelister);
   
-  console.log("HappyFansToken deployed to:", happyFun.address);
+  console.log("HappyFansToken deployed to:", happyFans.address);
 }
 
 main()
